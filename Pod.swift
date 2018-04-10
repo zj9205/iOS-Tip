1014a69a434fa2d0dc1c288ea28a6c1dd7d43de2
Jian Zhai <zhaijianjian@gmail.com>
2018-04-09 22:34:11 -0700

    update Pod.swift

--- Pod.swift
+++ Pod.swift
@@ -1,2 +1,16 @@
-// 注意：如出现 Unable to find a specification for 'some pod' 错误，可执行 pod repo update 命令。
+// install pod
+sudo gem install cocoapods
+pod setup --verbose
 
+// init pod
+pod init
+pod install
+pod update
+
+// open by Xcode
+open -a Xcode Podfile
+
+// pod version
+pod --version
+
+// 注意：如出现 Unable to find a specification for 'some pod' 错误，可执行 pod repo update 命令。
