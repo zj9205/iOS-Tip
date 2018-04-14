750ae5bc1b2f04eb04350931eaae700e39c0bc75
Jian Zhai <zhaijianjian@gmail.com>
2018-04-13 23:18:11 -0700

    Listening changes of a textField

--- UITextView.swift
+++ UITextView.swift
@@ -2,3 +2,10 @@
 // 防止边缘被切
 self.htmlTextView.isScrollEnabled = false
 self.htmlTextView.isScrollEnabled = true
+
+// Check if a UITextField is changing
+inputTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+
+@objc func textFieldDidChange(_ textField: UITextField) {
+    
+}
