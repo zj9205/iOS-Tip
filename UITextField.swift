268cf1d5358f8acc73d60fad48c7fa1f0114859b
Jian Zhai <zhaijianjian@gmail.com>
2018-04-16 22:35:40 -0700

    update UITextField

--- UITextField.swift
+++ UITextField.swift
@@ -2,3 +2,20 @@
 textfield.returnKeyType = .done
 textFieldDidEndEditing(_:) { textfield.resignFirstResponder }
 textFieldShouldEndEditing(_:) { textfield.resignFirstResponder; return true }
+
+// End editing
+func textFieldShouldReturn(textField: UITextField) -> Bool {
+    self.view.endEditing(true)
+    return true;
+}
+
+// Change Font
+textField.font = UIFont.init(name: "Helvetica", size: 18)
+
+// Change style of placeholder
+let attributes = [
+    NSForegroundColorAttributeName: UIColor.blackColor(),
+    NSFontAttributeName : UIFont(name: "Roboto-Bold", size: 17)!  // Note the !
+]
+someUITextField.attributedPlaceholder = NSAttributedString(string: "Placeholder Text", attributes:attributes)
+
