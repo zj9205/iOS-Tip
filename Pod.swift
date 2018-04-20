f70b575993da3ad3579216223d42d7c50df83baa
Jian Zhai <zhaijianjian@gmail.com>
2018-04-19 22:36:07 -0700

    update pod: set different swift version for different pods

--- Pod.swift
+++ Pod.swift
@@ -14,3 +14,14 @@ open -a Xcode Podfile
 pod --version
 
 // 注意：如出现 Unable to find a specification for 'some pod' 错误，可执行 pod repo update 命令。
+
+// pod with different swift version
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        if ['RxSwift', 'RxSwiftExt'].include? target.name
+            target.build_configurations.each do |config|
+                config.build_settings['SWIFT_VERSION'] = '3.2'
+            end
+        end
+    end
+end
