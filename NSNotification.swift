23820ffc23cec8e5a5e98af12bc50cbdf5096ac1
Jian Zhai <zhaijianjian@gmail.com>
2018-04-30 22:36:45 -0700

    NSNotification

--- NSNotification.swift
+++ NSNotification.swift
@@ -4,3 +4,21 @@ let keyboardSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectV
 let keyboardHeight = keyboardSize.height
 let intersection = self.view.frame.intersection(keyboardSize)
 
+// add, listen, get value, remove notification
+NotificationCenter.default.addObserver(self, selector: #selector(self.changePrice(notification:)), name: Notification.Name.init(rawValue: "changeDeliveryOption"), object: nil)
+NotificationCenter.default.post(name: Notification.Name.init(rawValue: "changeDeliveryOption"), object: nil, userInfo: ["index": row])
+@objc func changePrice(notification: Notification)  {
+    if let info = notification.userInfo {
+        let userInfo = info as! [String: Int]
+        if let product = product {
+            let indexPath = IndexPath(item: 0, section: 0)
+            if let cell = self.tableView.cellForRow(at: indexPath) {
+                let cell = cell as! ProductTableViewCell
+                cell.priceLabel.text = product.deliverList.fullPrice[userInfo["index"]!]
+            }
+        }
+    }
+}
+deinit {
+    NotificationCenter.default.removeObserver(self)
+}
