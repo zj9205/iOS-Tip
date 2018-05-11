fb45adeef20debd185e418801d5e324b3b9300e9
Jian Zhai <zhaijianjian@gmail.com>
2018-05-10 21:32:16 -0700

    update UINavigationController

--- UINavigationController.swift
+++ UINavigationController.swift
@@ -1,2 +1,7 @@
 // disable backe swipe gesture in UINavigationController
 self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+
+// add navigation controller to a controller
+let navigationController = UINavigationController(rootViewController: controller!)
+present(navigationController, animated: true, completion: nil)
+controller?.dismiss(animated: true, completion: nil)
