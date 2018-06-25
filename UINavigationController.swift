93ed5a6665f4a49be1012f85f21fd3f1a422b509
Jian Zhai <zhaijianjian@gmail.com>
2018-06-24 21:38:17 -0700

    update UINavigationController

--- UINavigationController.swift
+++ UINavigationController.swift
@@ -5,3 +5,9 @@ self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
 let navigationController = UINavigationController(rootViewController: controller!)
 present(navigationController, animated: true, completion: nil)
 controller?.dismiss(animated: true, completion: nil)
+
+// add button on navigation bar
+let filterButton = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterBarButtonPressed))
+let onboardButton = UIBarButtonItem(title: "Onboard", style: .plain, target: self, action: #selector(onboardButtonClicked))
+navigationItem.rightBarButtonItems = [onboardButton, filterButton]
+navigationItem.rightBarButtonItem = onboardButton
