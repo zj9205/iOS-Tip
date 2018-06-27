22235b9bd13a321358fba413d277eee03959e593
Jian Zhai <jian@go-goody.com>
2018-06-26 20:55:49 -0700

    add CALayer

---
+++ CALayer.swift
@@ -0,0 +1,9 @@
+// UIView, CALayer, set corner radius
+totalPriceContainerView.layer.cornerRadius = 20
+
+// UIView, CALayer, set shadow
+totalPriceContainerView.layer.shadowColor = totalPriceContainerView.backgroundColor?.cgColor
+totalPriceContainerView.layer.shadowOpacity = 1
+totalPriceContainerView.layer.shadowOffset = CGSize.zero
+totalPriceContainerView.layer.shadowRadius = 10
+totalPriceContainerView.layer.shouldRasterize = false
