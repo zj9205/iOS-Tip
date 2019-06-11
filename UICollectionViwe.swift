bfc54ab96a527a92dcc307a1694f02d744eafd29
Jian Zhai <zhaijianjian@gmail.com>
2019-06-11 16:44:01 -0700

    add objc code for reloading a specific cell

--- UICollectionViwe.swift
+++ UICollectionViwe.swift
@@ -1,9 +1,24 @@
 // Enable scroll or not
 self.collectionView.isScrollEnabled = false
 
-// reload all
+
+
+// reload all cells
 self.tableView.reloadData()
 
-// reload specific one
+
+
+// reload a specific cell
+
+// swift
 let indexPath = IndexPath(item: 0, section: 0)
 tableView.reloadRows(at: [indexPath], with: .none)
+
+// objc
+- (void)reloadItemsAtIndexPaths:(NSArray *)indexPaths
+
+// without animations
+BOOL animationsEnabled = [UIView areAnimationsEnabled];
+[UIView setAnimationsEnabled:NO];
+[myCollectionView reloadItemsAtIndexPaths:myIndexPaths];
+[UIView setAnimationsEnabled:animationsEnabled];
